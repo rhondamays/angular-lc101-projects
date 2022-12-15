df2be12da8acf310c75a225d64761eda7969af57
Rhonda Mays <maysrhonda.7@gmail.com>
2022-12-15 13:47:24 -0600

    added events to modify directives

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,54 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled: boolean = true;
+
+
+  handleTakeOff() {
+    let result = window.confirm("Are you sure the shuttle is ready for takeoff?")
+    if (result) {
+      this.color = "blue";
+      this.height = 10000;
+      this.width = 0;
+      this.message = "Shuttle in flight.";
+    }
+  }
+
+  handleLand() {
+    alert("The shuttle is landing. Landing gear engaged.");
+      this.message ="The shuttle has landed";
+      this.color = "green"
+      this.height = 0;
+  }
+
+  abortMission() {
+    let result = confirm("Are you sure you want to abort the mission?");
+    if (result) {
+      this.message = "Mission aborted";
+      this.color = "red";
+      this.height = 0;
+    }
+  }
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+   } else if(direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) - 10 + "px";
+      rocketImage.style.left= movement;
+      this.width = this.width - 10000;
+   } else if (direction === 'down') {
+    let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+    rocketImage.style.bottom = movement;
+    this.height = this.height - 10000;
+  } else {
+    let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+    rocketImage.style.bottom = movement;
+    this.height = this.height + 10000;
+  }
+  }
+
+
 }
