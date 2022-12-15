a86861fadb0ab3b967689e9b618db2f658aa8c33
Rhonda Mays <maysrhonda.7@gmail.com>
2022-12-15 10:20:49 -0600

    practiced attribute directives

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   //inactive: boolean = false;
+   goldInactive: boolean = false;
+   silverInactive = false;
+   copperInactive = false;
+
 
    constructor() { }
 
    ngOnInit() { }
 
+   resetButton() {
+      if (this.goldInactive && this.silverInactive && this.copperInactive) {
+         this.goldInactive = !this.goldInactive;
+         this.silverInactive = !this.silverInactive;
+         this.copperInactive = !this.copperInactive;
+      } else if (this.goldInactive && this.silverInactive) {
+         this.goldInactive = !this.goldInactive;
+         this.silverInactive = !this.silverInactive;
+      } else if (this.goldInactive && this.copperInactive) {
+         this.goldInactive = !this.goldInactive;
+         this.copperInactive = !this.copperInactive;
+      } else if (this.silverInactive && this.copperInactive) {
+         this.silverInactive = !this.silverInactive;
+         this.copperInactive = !this.copperInactive;
+      } else if (this.goldInactive) {
+         this.goldInactive = !this.goldInactive;
+      } else if (this.silverInactive) {
+         this.silverInactive = !this.silverInactive;
+      } else {
+         this.copperInactive = !this.copperInactive;
+      }
+   }
 }
